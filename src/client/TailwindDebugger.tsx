a186b54e2dfc8b854e6106d3215b97e6831da5bf
Keegan Donley <kd@keegandonley.com>
2024-04-12 09:14:43 -0500

    add tailwind styles

--- src/client/TailwindDebugger.tsx
+++ src/client/TailwindDebugger.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import "../../tw-output.css";
 
 export const TailwindDebugger = () => {
   const [dimensions, setDimensions] = React.useState({ width: 0, height: 0 });
