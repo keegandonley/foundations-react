f22de97941009b313da66ee9b2b9c59cec235089
Keegan Donley <kd@keegandonley.com>
2024-04-27 16:22:37 -0500

    allow arbitrary code to be copied

--- src/hooks/useCopyElementText.ts
+++ src/hooks/useCopyElementText.ts
@@ -1,6 +1,6 @@
 import { useCallback, useRef, useState } from "react";
 
-export const useCopyElementText = () => {
+export const useCopyElementText = (content?: string) => {
   const elementRef = useRef<HTMLElement>(null);
   const timeoutRef = useRef<NodeJS.Timeout | null>(null);
   const [clicked, setClicked] = useState(false);
@@ -11,13 +11,19 @@ export const useCopyElementText = () => {
     }
 
     if (!elementRef.current) {
+      if (content) {
+        navigator.clipboard?.writeText?.(content);
+        setClicked(true);
+        timeoutRef.current = setTimeout(() => setClicked(false), 1000);
+      }
+
       return;
     }
 
     navigator.clipboard?.writeText?.(elementRef.current.innerText);
     setClicked(true);
     timeoutRef.current = setTimeout(() => setClicked(false), 1000);
-  }, []);
+  }, [content]);
 
   return { ref: elementRef, onClick: handleCopyClick, pending: clicked };
 };
